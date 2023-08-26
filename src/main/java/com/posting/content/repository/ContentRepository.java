0d2d41df78f1d607bf4d1ccb95dfb2a304a0fdb3
seonhoshin <shinwder@gmail.com>
2023-08-26 10:42:22 +0900

    aaa

--- src/main/java/com/posting/content/repository/ContentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.posting.content.repository;
-
-import com.posting.content.entity.Content;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface ContentRepository extends JpaRepository<Content, Long> {
-
-}
